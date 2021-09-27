4e1d0082edf7fd8b42fe7cdf9d5a86a31845cffd
Heechang Yang <yangtopia@gmail.com>
2021-09-27 14:45:29 +0900

    style(my-app2): 텍스트 변경

--- packages/my-app2/src/App.tsx
+++ packages/my-app2/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 2
+          Learn React Second
         </a>
       </header>
     </div>
