cd0ac18a292ff197d8eb7a468ed806f8cac11c67
Heechang Yang <yangtopia@gmail.com>
2021-09-27 10:58:16 +0900

    chore: test

--- packages/my-app2/src/App.tsx
+++ packages/my-app2/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 23
+          Learn React 2
         </a>
       </header>
     </div>
